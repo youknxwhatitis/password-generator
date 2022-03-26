7b0e4d633d173d655d4b1db2bc57abfe001ce518
Erick Ramos Torres <Erickramos780@gmail.com>
2022-03-26 13:55:41 -0400

    Starter code

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var validatedNumber; 
+var validatedCharacter; 
+var validatedUppercase; 
+var validatedLowercase; 
+
+//Values for Characters 
+
+//character = ["!","'","#", "$","%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "\", "^", "{", "}", "~"]; 
+
+//number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+
+
+
+
+
+function generatePassword(){
+  console.log("Clicked Button"); 
+  // 1. Prompt the user for the password 
+  let person = prompt("What kind of characters would you like for your password? Decide between 8 and 128", "Lemon Chiffon"); 
+  if (!person) {
+    alert("Need a Value");
+  } else if (person < 8 || person > 128) {
+    // Validation 
+    person = prompt("You must choose between 8 and 128 characters")
+
+  } else {
+    validatedNumber = confirm("will it have numbers?");
+    validatedCharacter = confirm("will it have special characters?");
+    validatedUppercase = confirm("will it have uppercase letters?");
+    validatedLowercase = confirm ("Will it have Lowercase letters?")
+  }; 
+
+
+  }
+  // - password length 8 < 128
+  // - Lowercase, uppercase, number, special characters 
+  // 2. Validate the input 
+  // 3. Generate password based on criteria 
+  // 4. display password to the page.
+
+  return "Generated button"
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +58,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
